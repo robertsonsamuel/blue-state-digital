2e1a6144bc34f0b412acc234f28b437419a0a7e7
Samuel Robertson <samuel.robertson93@gmail.com>
2017-12-14 16:40:03 -0500

    Updated ListFormFields test to mock requests

--- test/signupForms/listFormFields.test.js
+++ test/signupForms/listFormFields.test.js
@@ -1,6 +1,23 @@
+import nock from 'nock'
+import path from 'path'
+import fs from 'fs'
+
 import BSD from './../../src'
 
 require('dotenv').config()
+
+beforeAll(() => {
+  nock(`https://${process.env.BSD_DEV}`)
+    .get('/page/api/signup/list_form_fields')
+    .query(true)
+    .reply(200, (uri, requestBody, cb) => {
+      fs.readFile(path.resolve(__dirname, '../fixtures/signupForms/listFormFields.xml'), cb)
+    })
+})
+
+afterAll(() => {
+  nock.cleanAll()
+})
 
 const blueStateDigital = new BSD({
   baseUrl: process.env.BSD_DEV,
