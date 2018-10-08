5f0fead34907724e31be5ecddcb27e9248771368
Jack Steadman <jsteadman@bluestatedigital.com>
2018-10-08 14:14:00 -0400

    updated documentation

--- src/sendTriggeredEmail.js
+++ src/sendTriggeredEmail.js
@@ -6,26 +6,12 @@ import { generateApiMac, getRequestPromise } from './utils'
 /**
  * Searches all the events and returns the future events unless specified by date_start.
  *
- * REF: https://secure.bluestatedigital.com/page/api/doc#-------------Event-API-Calls---------
+ * REF: https://secure.bluestatedigital.com/page/api/doc#-----------------send_triggered_email-------------
  * @param {object} params The BSD API Options.
- * @param {string} params.event_id Search by Event ID
- * @param {string} params.event_type Search by Event Type
- * @param {string} params.host_name Search by the Host of the Event
- * @param {string} params.day Search by the day of the Event
- * @param {string} params.date_start Search by the start date of the Event
- * @param {string} params.date_end Search by the end date of the Event
- * @param {string} params.create_day Search by the created day of the Event
- * @param {string} params.create_date_start Search by the created start date of the Event
- * @param {string} params.create_date_end Search by the created end date of the Event
- * @param {string} params.country Search by events country
- * @param {string} params.zip Search by events zipcode
- * @param {string} params.city Search by events by city
- * @param {string} params.state Search events by state
- * @param {string} params.zip_radius Search events in a given zipcode radius
- * @param {string} params.radius_unit Search events in a given radius
- * @param {string} params.attendee_cons_id Search events by a consituent's ID who is attending
- * @param {string} params.creator_cons_id Search events by the consituent that created the Event
- * @param {string} params.order_field Search events by order field
+ * @param {string} params.mailing_id Obfuscated mailing ID to be sent (found on the manage mailings page)
+ * @param {string} params.email Recipient email address
+ * @param {string} params.email_opt_in Whether to subscribe newly-created constituents
+ * @param {string} params.trigger_values URL-encoded json containing data to be made available to the email
  * @returns {Promise} BSD JSON Response Promise
  * @memberof BSD
  */
